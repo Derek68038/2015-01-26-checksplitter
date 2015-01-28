284d21d4342d6eb7630875515d0bf3074b77215e
Derek George <derek68038@gmail.com>
2015-01-27 19:52:01 -0600

    Added location method to show who went to the restaurant.
    Added method to allow one person to pay for everyone.

--- dinnerclub.rb
+++ dinnerclub.rb
@@ -2,27 +2,50 @@ require 'pry'
 
 class DinnerClub
 
-def initialize(group)
-  @group = group
-end
-
-def group_list
-  @group_list = {}
-  @group.each { |m| @group_list[m] = 0 }
-  @group_list
-end
-
-def event(person, bill)
-  @person = person
-  event_check = CheckSplitter.new(bill, person.length)
-  person.each do |a| 
-    @group_list[a] += event_check.per_person
+  def initialize(group)
+    @group = group
   end
-  @group_list
-end
+
+  def group_list
+    @group_list = {}
+    @group.each { |m| @group_list[m] = 0 }
+    @group_list
+  end
+
+  def event(place, person, bill)
+    @place = place
+    @person = person
+    event_check = CheckSplitter.new(bill, person.length)
+    person.each do |a| 
+      @group_list[a] += event_check.per_person
+    end
+    @group_list
+  end
+
+  def location
+    location = {}
+    location[@place] = @person
+    location
+  end
+
+  def treat_everyone(place, person, bill, pay)
+    @place = place
+    @person = person
+    @pay = pay
+    event_check = CheckSplitter.new(bill, person.length)
+    person.each do |a| 
+      if a = pay
+        group_list[a] += event_check.total
+      else
+        a = 0
+      end
+    end
+   @group_list
+  end
 end
 
 class CheckSplitter
+  
   def initialize(cost, people) #initialized both the cost & # of people
     @cost = cost
     @tip = @cost * 0.2 #set default value for tip
@@ -51,11 +74,14 @@ class CheckSplitter
   end
 end 
 
-group = DinnerClub.new(["Derek","Hilary","Brett","Dawn"]) 
-group.group_list
-group.event(["Derek", "Hilary", "Brett", "Dawn"], 150)
-group.event(["Derek", "Hilary"], 50)
-group.event(["Derek", "Brett"], 200)
+fun_group = DinnerClub.new(["Derek","Hilary","Brett","Dawn"]) 
+fun_group.group_list
+fun_group.treat_everyone("Lot 2", ["Derek", "Hilary", "Brett", "Dawn"], 150, "Derek")
+fun_group.location
+
+dull_group = DinnerClub.new(["Bill", "Tom", "Jason"])
+dull_group.group_list
+dull_group.treat_everyone("Burger King", ["Bill", "Tom"], 35, "Tom")
 
 binding.pry
   
