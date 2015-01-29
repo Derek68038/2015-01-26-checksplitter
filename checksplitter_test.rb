a10db8601904414c9ce638eff8696126f7564374
Derek George <derek68038@gmail.com>
2015-01-28 18:00:43 -0600

    Learned how to test a program.

--- checksplitter_test.rb
+++ checksplitter_test.rb
@@ -5,6 +5,12 @@ class CheckSplitterTest < Minitest::Test
   
   def test_simple_input_with_expected_integer_output
     cs = CheckSplitter.new(100, 4)
-    assert_equal(35, cs.return)
+    assert_equal(30, cs.per_person)
+  end
+
+  def test_if_tax_is_default_value
+    check = CheckSplitter.new(100, 4)
+    assert_equal(20, check.tip)
   end
 end
+  
